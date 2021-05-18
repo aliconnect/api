3550f3bfc5713e8e80065697e50cfe4988375681
Max van Kampen <max.van.kampen@alicon.nl>
2021-05-18 13:55:23 +0200

    Auto commit

---
+++ sql/aim1/attribute.vw1.sql
@@ -0,0 +1,16 @@
+﻿USE [aim1]
+GO
+ALTER VIEW [attribute].[vw1] AS
+	SELECT
+	attributeID AS aid,
+	itemId AS id,
+	AttributeName AS name,
+	value,
+	hostID,
+	CreatedDateTime AS createdDT,
+	LastModifiedDateTime AS modDT,
+	userID,
+	nameid as fieldID,
+	linkid as itemID
+	FROM attribute.vw
+GO
